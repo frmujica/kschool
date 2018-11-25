dfa87570628f96b932d2b88e1c0e04c79892f143
Francisco <frmujica@outlook.es>
2018-11-25 23:24:29 +0100

    Mensaje de Commit

--- README.md
+++ README.md
@@ -8,3 +8,38 @@ Kschool Environment
 <li>Fecha:	23/11/2018</li>
 
 <hr>
+
+<li>publicar_GHub.sh carpeta_a_publicar
+
+
+<li>Este admite como parametro la ruta de una carpeta.
+
+<li>El script entrará en la carpeta, recorrerá recursivamente ficheros y subcareptas y las añadira a la tu GITHUB
+
+<li>Finalmente para publicar en GITHUB nos pedira nuestro uusuario y nuestra clave de GITHUB
+
+<br>
+<br>
+
+Antes de probar el script, debes de editar el fihcero y asignar tu nombre y correo de GITHUB correspondiente y a las variables "NOMBRE" e "EMAIL" respectivamente.
+
+<br>
+<br>
+
+Ejemplo:
+
+<br>
+<br>
+
+Si nuestros projectos se encuentra en la carpeta "/home/dsc/Repos/kschool/"
+<br>
+Copiaremos este script en este carpeta "/home/dsc/Repos/"
+<br>
+Y ejecutaremos <b>"./publicar_GHUB.sh kschool"</b>
+
+
+
+
+
+
+
---
+++ publicar_GHub.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+DIRECTORIO=""
+NOMBRE="Francisco Mujica"
+EMAIL="frmujica@outlook.es"
+
+
+# CHEQUEAMOS QUE TENEMOS UN NOMBRE DE USUARIO DE GITHUB CONFIGURADO
+if [ "$NOMBRE" = "" ]
+then
+ 	echo "Edita este script y asigna valor a la variable NOMBRE con tu nombre en GITHUB"
+	exit 1
+fi
+
+# CHEQUEAMOS QUE TENMEOS UNA CUANTA DE CORREO PARA GITHUB
+if [ "$EMAIL" = "" ]
+then
+ 	echo "Edita este script y asigna valor a la variable EMAIL con tu correo en GITHUB"
+	exit 1
+fi
+
+# COMPROBAMOS QUE TENEMOS UN PARAMETRO DE ENTRADA
+if [ "$#" = 0 ] 
+then
+	echo "Debe de indicar la carpeta en la que se encuentra el projecto a publicar"
+	exit
+else
+  	DIRECTORIO=$1
+
+	# COMPROBAMOS QUE PODEMOS ENTAR EN LA CARPATA DE ENTRADA DONDE ESTA NUESTRO PROYECTO
+	if ! cd $DIRECTORIO
+	then
+		echo "Carpeta no valida"
+		exit 1  
+	else 
+		echo "Ruta a cargar en GITHUB: $PWD"
+	fi
+ 
+fi
+
+
+# COFIGURAMOS EL ENTORNA PARa EL USUARIO CONFIGURADO
+git config --global user.name $NOMBRE
+git config --global user.email $EMAIL
+
+# AÑADIMOS LOSFICHEROS Y CARPETAS DE LA RUTA QUE NOS HA ENTRADO COMO PARAMETRO
+# for i in $(find . -maxdepth 1)
+for i in $(find . -not -path '*/\.*')
+do
+	if [ $i != "." ] && [ $i != ".."  ]
+	then
+		echo "Añadido: $i"
+		git add $i --force
+	fi
+#	
+done
+
+# COMMIT
+git commit -m "Mensaje de Commit"
+
+# PUSH. NOS PEDIRA USUARIO Y CLAVE DE GITHUB
+git push
+
+
+
+
